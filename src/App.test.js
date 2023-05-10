0faa64c47fd8578e48e4aa9eafabc3622161210a
raikiri1 <aibecka2@gmail.com>
2023-05-11 01:35:27 +0600

    Updates

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import App from './App';
-import { ReactDOM } from 'react-dom/client';
-
-it('renders without crashing', () => {
-  const div = document.createElement ('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
